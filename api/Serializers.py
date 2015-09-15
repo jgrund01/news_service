2ad2abc1e07f040b15c72a140bf77ee53877bd09
Jordan Grund <jordan.grund@adherial.com>
2015-09-14 21:09:13 -0700

    Added initial model and serializer for news feeds and news items

---
+++ api/Serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from api.models import NewsFeed, NewsItem
+
+
+class NewsFeedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsFeed
+        fields = ('url', 'md5_id')
+
+
+class NewsItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsItem
+        fields = ('news_text', 'news_feed')
--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+
+class NewsFeed(models.Model):
+
+    url = models.URLField(max_length=255)
+    md5_id = models.CharField(max_length=255)
+
+    created = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
+
+
+class NewsItem(models.Model):
+
+    news_text = models.TextField()
+
+    news_feed = models.ForeignKey(NewsFeed)
+    created = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
