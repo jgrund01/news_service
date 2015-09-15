e5c93bc1085f580353058e02fd1d0f44f7d93779
Jordan Grund <jordan.grund@adherial.com>
2015-09-14 21:19:55 -0700

    Added view for rendering news feeds in json

--- api/views.py
+++ api/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from api.models import NewsFeed, NewsItem
+from api.Serializers import NewsFeedSerializer, NewsItemSerializer
 
-# Create your views here.
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def news_feed_list(request):
+    if request.method == 'GET':
+        news_feeds = NewsFeed.objects.all()
+        serializer = NewsFeedSerializer(news_feeds, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = NewsFeedSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
